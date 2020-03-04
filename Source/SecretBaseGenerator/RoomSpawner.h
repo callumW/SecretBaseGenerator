9d9faa738f2dd08ce825260b9381d8f7a6ab2c01
Callum Wilson <calwilson@live.co.uk>
2020-03-04 21:18:39 +0000

    Added spawning of mesh at compile time

---
+++ Source/SecretBaseGenerator/RoomSpawner.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Room.h"
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RoomSpawner.generated.h"
+
+UCLASS()
+class SECRETBASEGENERATOR_API ARoomSpawner : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ARoomSpawner();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	ARoom* room = nullptr;
+};
